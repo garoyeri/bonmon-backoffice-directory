803bba8c1dd3cd5cd2196d7cfcf43111096ebd2f
Garo Yeriazarian <garo.yeriazarian@headspring.com>
2019-11-12 22:26:22 -0600

    Refactor Weather feature to feature slice

--- Directory/Features/Weather/WeatherForecast.cs
+++ Directory/Features/Weather/WeatherForecast.cs
@@ -1,15 +1,56 @@
 namespace Directory.Features.Weather
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
 
-    public class WeatherForecast
+    public static class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-        public int TemperatureC { get; set; }
+        public class Query : IRequest<Response>
+        {
+            public Query(int numberOfDays = 5) => NumberOfDays = numberOfDays;
+            public int NumberOfDays { get; set; }
+        }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public class Response
+        {
+            public Response() => Days = new List<Day>();
+            public Response(IEnumerable<Day> days) => Days = days.ToList();
+            public List<Day> Days { get; set; }
+        }
 
-        public string? Summary { get; set; }
+        public class Day
+        {
+            public DateTime Date { get; set; }
+            public int TemperatureC { get; set; }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+            public string? Summary { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Response>
+        {
+            public Task<Response> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var rng = new Random();
+                return Task.FromResult(new Response(
+                    Enumerable.Range(1, request.NumberOfDays)
+                        .Select(index => new Day
+                        {
+                            Date = DateTime.Now.AddDays(index),
+                            TemperatureC = rng.Next(-20, 55),
+                            Summary = Summaries[rng.Next(Summaries.Length)]
+                        })
+                    )
+                );
+            }
+        }
     }
 }
